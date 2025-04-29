59247a924dcae8e57d4f7cdba9a85d4f1ca18ddc
Richard Buckley <42151145+rhbuckley@users.noreply.github.com>
2025-04-29 18:03:37 -0400

    chore: add script to update instruct_data in 7B.yaml with JSONL files from specified directory

---
+++ src_finetune/config/generate.py
@@ -0,0 +1,34 @@
+import os
+
+
+if __name__ == "__main__":
+    lines = []
+    with open("7B.yaml", "r") as f:
+        # find the instruct_data key
+        i = 0
+        csf = None
+        for i, line in enumerate(f.readlines()):
+            if "instruct_data: " not in line:
+                continue
+
+            # split the line into key and value
+            key, value = line.split(": ")
+
+            # find all files within the directory
+            dir = "/work/pi_mchiovaro_uri_edu/richard_buckley/musicgen/jsonl_data"
+            files = [f for f in os.listdir(dir) if f.endswith(".jsonl")]
+            csf = ",".join(files)
+            break
+
+        if csf is None:
+            raise ValueError("instruct_data not found")
+
+        lines = f.readlines()
+
+        # replace the line with the new value
+        lines[i] = lines[i].split(": ")[0] + ": " + f'"{csf}"' + "\n"
+
+    with open("7B.yaml", "w") as f:
+        f.writelines(lines)
+
+    print("✅ Done!")
